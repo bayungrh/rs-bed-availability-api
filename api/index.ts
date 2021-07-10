12fae96641f577e522761165822f47a66cce78f8
bayungrh <bnugraha00@gmail.com>
2021-07-10 19:12:59 +0700

    add repo link

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,8 @@ export default (_: VercelRequest, response: VercelResponse) => {
     endpoint: {
       covid: '/api/covid/[id_propinsi]/[id_kota]',
       non_covid: '/api/non-covid/[id_propinsi]/[id_kota]',
-      detail: '/api/bed-detail/[rs_id]'
-    }
+      detail: '/api/bed-detail/[hospital_code]'
+    },
+    repository: 'https://github.com/bayungrh/rs-bed-availability-api'
   });
 }
