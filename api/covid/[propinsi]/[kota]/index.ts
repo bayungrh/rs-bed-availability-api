f4d188578c551cc92a3424fa584ea9399f7f86f5
bayungrh <bnugraha00@gmail.com>
2021-07-13 12:02:43 +0700

    add cache

--- api/covid/[propinsi]/[kota]/index.ts
+++ api/covid/[propinsi]/[kota]/index.ts
@@ -1,19 +1,32 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { fetchCovid } from '../../../../util/api';
 import { extractInfoCovid } from '../../../../util/helper';
+import cache from 'memory-cache';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   try {
     const { propinsi, kota } = request.query;
+    const cacheKey = `API_COVID_${propinsi}_${kota}`;
+    const cacheValue = cache.get(cacheKey);
+    console.log(cacheValue)
+    if(cacheValue) {
+      return response.status(200).json(JSON.parse(cacheValue));
+    }
+
     const res = await fetchCovid('rumah_sakit', {
       propinsi: propinsi ? `${propinsi}prop` : '',
       kabkota: kota == '0' ? '' : kota
     });
     const rsList = extractInfoCovid(res.body, request.query);
-    response.json({
+    const jsonResponse = {
       type: 'covid',
       result: rsList
-    });
+    };
+
+    // cache for 5min
+    cache.put(cacheKey, JSON.stringify(jsonResponse));
+
+    response.json(jsonResponse);
   } catch (error) {
     response.status(500).json({
       statusCode: 500,
